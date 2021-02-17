9cbb379e77f06b504849d8c6dd17b9fb14068f29
Adam Serraj Hassan <ads00067@students.stir.ac.uk>
2021-02-17 11:16:11 +0100

    Implemented the Title Case version as well as the upper case version

--- FilesInOut.java
+++ FilesInOut.java
@@ -12,23 +12,79 @@ import java.lang.Number;
  */
 public class FilesInOut {
 
-    public static void main(String[] args) {
-        // Replace this with statements to set the file name (input) and file name (output).
-        // Initially it will be easier to hardcode suitable file names.
+    public static void main(String[] args) throws FileNotFoundException {
 
-        // Set up a new Scanner to read the input file.
-        // Processing line by line would be sensible here.
-        // Initially, echo the text to System.out to check you are reading correctly.
-        // Then add code to modify the text to the output format.
+        // checks if the args array contains the -u flag
+        boolean hasUFlag = Arrays.stream(args).anyMatch("-u"::equals);
 
-        // Set up a new PrintWriter to write the output file.
-        // Add suitable code into the above processing (because you need to do this line by line also.
-        // That is, read a line, write a line, loop.
-
-        // Finally, add code to read the filenames as arguments from the command line.
-
-        System.out.println("You need to add your own code to do anything");
+        if (hasUFlag) {
+            upperCase(args[1], args[2]);
+        } else {
+            titleCase(args[0], args[1]);
+        }
 
     } // main
+
+    /**
+     *
+     * @param inputFile - File to be read from
+     * @param outputFile - File to be written to
+     * @throws FileNotFoundException
+     */
+    public static void titleCase(String inputFile, String outputFile) throws FileNotFoundException {
+        File input = new File(inputFile);
+        File output = new File(outputFile);
+        PrintWriter printWriter = new PrintWriter(output);
+
+        Scanner in = new Scanner(input);
+        while (in.hasNextLine()) {
+            Scanner in2 = new Scanner(in.nextLine());
+            // read all the inputs in a line
+            while (in2.hasNext()) {
+                String lineInput = in2.next();
+                /* capitalise the first character if it is a letter, or add forward slashes if
+                    a number is spotted*/
+                if (Character.isLetter(lineInput.charAt(0))) {
+                    lineInput = Character.toUpperCase(lineInput.charAt(0)) + lineInput.substring(1);
+                } else {
+                    lineInput = lineInput.substring(0, 2) + "/" + lineInput.substring(2, 4) + "/" + lineInput.substring(4, 8);
+                }
+                printWriter.print(lineInput + " ");
+            }
+            printWriter.println();
+        }
+        printWriter.close();
+    }
+
+    /**
+     *
+     * @param inputFile - File to be read from
+     * @param outputFile - File to be written to
+     * @throws FileNotFoundException
+     */
+    public static void upperCase(String inputFile, String outputFile) throws FileNotFoundException {
+        File input = new File(inputFile);
+        File output = new File(outputFile);
+        PrintWriter printWriter = new PrintWriter(output);
+
+        Scanner in = new Scanner(input);
+        while (in.hasNextLine()) {
+            Scanner in2 = new Scanner(in.nextLine());
+            // read all the inputs in a line
+            while (in2.hasNext()) {
+                String lineInput = in2.next();
+                /* capitalise the whole input if it is a letter, or add forward slashes if
+                    a number is spotted*/
+                if (Character.isLetter(lineInput.charAt(0))) {
+                    lineInput = lineInput.toUpperCase();
+                } else {
+                    lineInput = lineInput.substring(0, 2) + "/" + lineInput.substring(2, 4) + "/" + lineInput.substring(4, 8);
+                }
+                printWriter.print(lineInput + " ");
+            }
+            printWriter.println();
+        }
+        printWriter.close();
+    }
 
 } // FilesInOut
