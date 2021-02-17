94ee9fc2dac1b0fcf0cdbf09b9a30b0bae3ef749
stephanie-gelder <slg00016@students.stir.ac.uk>
2021-02-17 11:12:45 +0000

    Input/output files created/check to see if already exist

--- FilesInOut.java
+++ FilesInOut.java
@@ -1,4 +1,4 @@
-import java.io.*;
+import java.io.*; //this package contains all the classes sed in file processing
 import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
@@ -16,18 +16,57 @@ public class FilesInOut {
         // Replace this with statements to set the file name (input) and file name (output).
         // Initially it will be easier to hardcode suitable file names.
 
+        //creates an input file and checks to see if it already exists
+        try {
+            File inputFile = new File ("input.txt");
+            if (inputFile.createNewFile()){
+                System.out.println("File " + inputFile.getName() + " created in: "+ inputFile.getAbsolutePath());
+            } else
+                System.out.println("inputFile already exists");
+        } catch (IOException e){
+            System.out.println("An error occurred");
+            e.printStackTrace();
+        }
+        //creates an output file and checks to see if it already exists
+        try {
+            File outputFile = new File ("formatted.txt");
+            if (outputFile.createNewFile()){
+                System.out.println("File " + outputFile.getName() + " created in: "+ outputFile.getAbsolutePath());
+            } else
+                System.out.println("outputFile already exists");
+        } catch (IOException e){
+            System.out.println("An error occurred");
+            e.printStackTrace();
+        }
         // Set up a new Scanner to read the input file.
         // Processing line by line would be sensible here.
         // Initially, echo the text to System.out to check you are reading correctly.
         // Then add code to modify the text to the output format.
 
+
+
         // Set up a new PrintWriter to write the output file.
         // Add suitable code into the above processing (because you need to do this line by line also.
         // That is, read a line, write a line, loop.
+//        System.out.println("supply filename for input:");
+//        try {
+//            inputFile= in.nextLine();
+//            //File inputFile = new File(inputFile) ;
+//            Scanner inFile = new Scanner(inputFile);
+//        } catch (IOException e) {
+//            System.err.println("IOException: " + e.getMessage() + “ not found”);
+//        }
+//
+//        System.out.println("supply filename for output:");
+//        try {
+//            outputFile = new PrintWriter(filename);
+//        } catch (FileNotFoundException e) {
+//            System.err.println("FileNotFoundException: " + e.getMessage() + " not openable");
+//            System.exit(0);
+//        }
 
         // Finally, add code to read the filenames as arguments from the command line.
 
-        System.out.println("You need to add your own code to do anything");
 
     } // main
 
