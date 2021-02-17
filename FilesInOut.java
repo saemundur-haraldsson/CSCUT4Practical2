90f44dae1d5034c58da4b41c952127a41b732d39
stephanie-gelder <slg00016@students.stir.ac.uk>
2021-02-17 18:33:31 +0000

    Formatting the input file for -u and title case as well as the date (doesn't fully work)

--- FilesInOut.java
+++ FilesInOut.java
@@ -1,6 +1,8 @@
 import java.io.*; //this package contains all the classes sed in file processing
 import java.awt.*;
 import java.awt.event.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 import javax.swing.*;
 import java.lang.Number;
@@ -13,38 +15,77 @@ import java.lang.Number;
 public class FilesInOut {
 
     public static void main(String[] args) {
-        // Replace this with statements to set the file name (input) and file name (output).
-        // Initially it will be easier to hardcode suitable file names.
-
         //creates an input file and checks to see if it already exists
         try {
-            File inputFile = new File ("input.txt");
-            if (inputFile.createNewFile()){
-                System.out.println("File " + inputFile.getName() + " created in: "+ inputFile.getAbsolutePath());
+            File inputFile = new File("input.txt");
+            if (inputFile.createNewFile()) {
+                System.out.println("File " + inputFile.getName() + " created in: " + inputFile.getAbsolutePath());
             } else
                 System.out.println("inputFile already exists");
-        } catch (IOException e){
+        } catch (IOException e) {
             System.out.println("An error occurred");
             e.printStackTrace();
         }
         //creates an output file and checks to see if it already exists
         try {
-            File outputFile = new File ("formatted.txt");
-            if (outputFile.createNewFile()){
-                System.out.println("File " + outputFile.getName() + " created in: "+ outputFile.getAbsolutePath());
+            File outputFile = new File("formatted.txt");
+            if (outputFile.createNewFile()) {
+                System.out.println("File " + outputFile.getName() + " created in: " + outputFile.getAbsolutePath());
             } else
                 System.out.println("outputFile already exists");
-        } catch (IOException e){
+        } catch (IOException e) {
             System.out.println("An error occurred");
             e.printStackTrace();
         }
-        // Set up a new Scanner to read the input file.
-        // Processing line by line would be sensible here.
         // Initially, echo the text to System.out to check you are reading correctly.
         // Then add code to modify the text to the output format.
 
+        String inFileName = "";
+        String upperCaseFormat = "";
+        String outFileName = "";
 
-
+        if (args[0].equals("-u")) {
+            upperCaseFormat = args[0];
+            inFileName = args[1];
+            outFileName = args[2];
+        } else {
+            inFileName = args[0];
+            outFileName = args[1];
+        }
+        try {
+            File inputFile = new File(inFileName);
+            Scanner inFile = new Scanner(inputFile);
+            BufferedReader reader = new BufferedReader(new FileReader(inputFile)); //reads text fm a char-input stream
+            String line = reader.readLine();
+            Date d = null;
+            while (line != null) {
+                SimpleDateFormat origFormat = new SimpleDateFormat("ddMMyyyy"); //formats what is currently in the file
+                SimpleDateFormat newFormat = new SimpleDateFormat("dd/MM/yyy"); //changes it to the format we want
+                String value = line.replaceAll("[^0-9]", ""); //selects the date and removes any int (regular expression pattern finding)
+                d = origFormat.parse(value); //parse date fm original to new format
+                String name = line.replaceAll("[\\d.]", ""); //selects the name and erases any non-int (regular expression pattern finding)
+                if (upperCaseFormat.equals("-u")) { //if the flag is '-u' then changes name to upper case
+                    name = name.toUpperCase();
+                } else {
+                    name = name.substring(0, 1).toUpperCase() + name.substring(1);
+                }
+                appendData(name, outFileName, newFormat, d); //append the data to the new file
+                line = reader.readLine(); //go to the nest line in the file
+            }
+        } catch (IOException | ParseException e) {
+            System.err.println("IOException: " + e.getMessage() + "not found");
+        }
+    }
+    public static void appendData(String name, String outFileName, SimpleDateFormat newFormat, Date d){
+        try {
+            FileWriter fw = new FileWriter(outFileName, true); //true will append new data
+            fw.write(name + newFormat.format(d)); //appends the string to the file
+            fw.write("\n"); //new line
+            fw.close();
+            //line = reader.readLine();
+        } catch (IOException e){
+            System.err.println("IOException: " + e.getMessage());
+        }
         // Set up a new PrintWriter to write the output file.
         // Add suitable code into the above processing (because you need to do this line by line also.
         // That is, read a line, write a line, loop.
