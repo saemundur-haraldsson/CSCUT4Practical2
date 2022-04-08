c7732748fdce4f4b819f6d1e7966f54f5d7d8ce7
dab00175 <dab00175@students.stir.ac.uk>
2022-04-08 18:50:22 +0100

    No middle name processing yet

--- FilesInOut.java
+++ FilesInOut.java
@@ -4,31 +4,93 @@ import java.awt.event.*;
 import java.util.*;
 import javax.swing.*;
 import java.lang.Number;
-
+import java.util.Scanner;
 /**
  * 
  * CSCU9T4 Java strings and files exercise.
+ * AUTHOR: 2924912
  *
  */
 public class FilesInOut {
 
     public static void main(String[] args) {
-        // Replace this with statements to set the file name (input) and file name (output).
-        // Initially it will be easier to hardcode suitable file names.
+    	String currentLine = ""; // variable for storing the line currently read by scanner
+    	String name = ""; // variable for storing the name in the current line
+    	String date = ""; // variable for storing the date in the current line
+    	ArrayList<String> names = new ArrayList<String>(); // ArrayList for the formatted names
+    	ArrayList<String> dates = new ArrayList<String>(); // ArrayList for the formatted dates
+    	
+    	try {
+    	String inputFileName = args[0];
+    	System.out.println("Input from file " + inputFileName);
+    	File inputFile = new File(inputFileName);
+        Scanner inFile = new Scanner(inputFile);
+        
+        while (inFile.hasNextLine()) {
+            currentLine = inFile.nextLine(); // current line read from file
+        	String[] splitData = currentLine.split("(?=\\d)(?<!\\d)"); // splits the string into a name and a date string, stores in an array
+        	name = splitData[0];
+        	date = splitData[1];
+            
+        	if (args.length == 3 && args[2].compareTo("-u") == 0) { // if there is a -u flag
+        		System.out.println("test");
+        		names.add(allCaps(name)); // calls the method for turning names to upper case
+        		dates.add(formatDate(date)); // calls for a method that formats the date
+        	}
+        	
+        	else {
+        	System.out.println("test2");
+        	names.add(formatName(name)); // calls a method that turns the first letters of the names to upper case
+        	dates.add(formatDate(date)); // calls for a method that formats the date
+        	}
+        }
+        inFile.close();
+    	}
+    	catch (IOException e) {
+    	System.err.println("IOException: " + e.getMessage() + "not found");
+    	}
+    	
+    	try {
+    	String filename = args[1];
+    	System.out.println("Writing to file " + filename);
+    	File outFile = new File(filename);
+    	PrintWriter writer = new PrintWriter(filename);
+    	
+    	for (int i = 0; i < names.size(); i++) {
+        	writer.printf("%-20s%20s\n", names.get(i), dates.get(i)); // writes the data to the file
+    	}
 
-        // Set up a new Scanner to read the input file.
-        // Processing line by line would be sensible here.
-        // Initially, echo the text to System.out to check you are reading correctly.
-        // Then add code to modify the text to the output format.
-
-        // Set up a new PrintWriter to write the output file.
-        // Add suitable code into the above processing (because you need to do this line by line also.
-        // That is, read a line, write a line, loop.
-
-        // Finally, add code to read the filenames as arguments from the command line.
-
-        System.out.println("You need to add your own code to do anything");
-
+    	writer.close();
+    	}
+    	catch (FileNotFoundException e) {
+    	System.err.println("FileNotFoundException: " + e.getMessage() + " not openable");
+    	System.exit(0); 
+    	}
     } // main
+    
+	public static String formatName(String name) {
+    	
+    	String splitName[] = name.split(" "); // splits the name into first and last names for easier capitalization
+    	String upperCase = "";
+    
+    	for (int i = 0; i < splitName.length; i++) {
+    		char firstLetter = splitName[i].charAt(0); // gets the first letter of each array element
+    		char big = Character.toUpperCase(firstLetter); // capitalizes the first letter
+    		upperCase += big + splitName[i].substring(1) + " "; // adds the capitalized first and last name back together
+    	}
+    	upperCase = upperCase.trim(); // removes the trailing space behind the name
+		
+		return upperCase;
+	}
+	
+	public static String formatDate(String date) {
+		date = date.replaceAll("(\\d{2})(\\d{2})(\\d{4})", "$1/$2/$3"); // adds slashes to the dates
+		return date;
+	}
+	
+	public static String allCaps (String data) {
+		data = data.toUpperCase(); // turns the data to all upper case
+		return data;
+	}
 
 } // FilesInOut
