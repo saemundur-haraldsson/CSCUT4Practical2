85831e1c9eaf699498aaee3218712cb59d4fd78b
dab00175 <dab00175@students.stir.ac.uk>
2022-04-08 21:03:36 +0100

    added middle name processing

--- FormatNamesm.java
+++ FormatNamesm.java
@@ -1,24 +1,17 @@
-import java.io.*;
-import java.awt.*;
-import java.awt.event.*;
-import java.util.*;
-import javax.swing.*;
-import java.lang.Number;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Scanner;
-/**
- * 
- * CSCU9T4 Java strings and files exercise.
- * AUTHOR: 2924912
- *
- */
-public class FilesInOut {
-//remove this...ss
-    public static void main(String[] args) {
+
+public class FormatNamesm {
+	public static void main(String[] args) {
     	String currentLine = ""; // variable for storing the line currently read by scanner
     	String name = ""; // variable for storing the name in the current line
     	String date = ""; // variable for storing the date in the current line
     	ArrayList<String> names = new ArrayList<String>(); // ArrayList for the formatted names
-    	ArrayList<String> dates = new ArrayList<String>(); // ArrayList for the formatted dates
+    	ArrayList<String> dates = new ArrayList<String>(); // ArrayList for the formatted date
     	
     	try {
     	String inputFileName = args[0];
@@ -33,13 +26,11 @@ public class FilesInOut {
         	date = splitData[1];
             
         	if (args.length == 3 && args[2].compareTo("-u") == 0) { // if there is a -u flag
-        		System.out.println("test");
         		names.add(allCaps(name)); // calls the method for turning names to upper case
         		dates.add(formatDate(date)); // calls for a method that formats the date
         	}
         	
         	else {
-        	System.out.println("test2");
         	names.add(formatName(name)); // calls a method that turns the first letters of the names to upper case
         	dates.add(formatDate(date)); // calls for a method that formats the date
         	}
@@ -74,6 +65,10 @@ public class FilesInOut {
     	String upperCase = "";
     
     	for (int i = 0; i < splitName.length; i++) {
+    		if (splitName[i].length() == 1) {
+    			splitName[i] = splitName[i] + ".";
+    		}
+    		
     		char firstLetter = splitName[i].charAt(0); // gets the first letter of each array element
     		char big = Character.toUpperCase(firstLetter); // capitalizes the first letter
     		upperCase += big + splitName[i].substring(1) + " "; // adds the capitalized first and last name back together
@@ -89,8 +84,18 @@ public class FilesInOut {
 	}
 	
 	public static String allCaps (String data) {
-		data = data.toUpperCase(); // turns the data to all upper case
+		String splitName[] = data.split(" "); // splits the name into first and last names for easier capitalization
+		String upperCase = "";
+	    
+    	for (int i = 0; i < splitName.length; i++) {
+    		if (splitName[i].length() == 1) { // if a middle initial is found
+    			splitName[i] = splitName[i] + "."; // adds a period after the middle name
+    		}
+    		upperCase += splitName[i] +  " "; // adds the capitalized first and last name back together
+    	}
+    	upperCase = upperCase.trim(); // removes the trailing space behind the name
+		
+		data = upperCase.toUpperCase(); // turns the data to all upper case
 		return data;
 	}
-
-} // FilesInOut
+}
