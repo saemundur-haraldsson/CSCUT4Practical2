451aab6116b57531fb98a1dc426f50e16ba24048
louiefk <lfk00001@students.stir.ac.uk>
2023-04-21 04:05:48 +0100

    Task 3

--- FilesInOut.java
+++ FilesInOut.java
@@ -1,80 +1,73 @@
 import java.io.*;
 import java.util.*;
 
-/**
- * 
- * CSCU9T4 Java strings and files exercise.
- *
- */
 public class FilesInOut {
 
     public static void main(String[] args) {
         String inputFileName, outputFileName;
-        boolean isUpperCase = false;
-
-        // Check command line arguments.
-        if (args.length < 2 || args.length > 3) {
-            System.err.println("Usage: java FilesInOut [-u] input.txt output.txt");
-            System.exit(1);
+        Scanner scanner = new Scanner(System.in);
+        BufferedReader reader = null;
+        BufferedWriter writer = null;
+        
+        // Ask the user for the input file name and keep asking until a valid file is given
+        while (true) {
+            System.out.print("Enter the input file name: ");
+            inputFileName = scanner.nextLine();
+            try {
+                reader = new BufferedReader(new FileReader(inputFileName));
+                break;
+            } catch (FileNotFoundException e) {
+                System.out.println("File not found. Try again.");
+            }
         }
 
-        int argIndex = 0;
-        if (args.length == 3 && args[argIndex].equals("-u")) {
-            isUpperCase = true;
-            argIndex++;
+        // Ask the user for the output file name and keep asking until a valid file is given
+        while (true) {
+            System.out.print("Enter the output file name: ");
+            outputFileName = scanner.nextLine();
+            try {
+                writer = new BufferedWriter(new FileWriter(outputFileName));
+                break;
+            } catch (IOException e) {
+                System.out.println("Could not open file. Try again.");
+            }
         }
-        inputFileName = args[argIndex++];
-        outputFileName = args[argIndex];
 
         // Set up a new Scanner to read the input file.
-        Scanner scanner = null;
+        // Processing line by line would be sensible here.
+        // Initially, echo the text to System.out to check you are reading correctly.
+        // Then add code to modify the text to the output format.
+        String line;
         try {
-            scanner = new Scanner(new File(inputFileName));
-        } catch (FileNotFoundException e) {
-            System.err.println("Input file not found: " + inputFileName);
-            System.exit(1);
-        }
-
-        // Set up a new PrintWriter to write the output file.
-        PrintWriter writer = null;
-        try {
-            writer = new PrintWriter(new File(outputFileName));
-        } catch (FileNotFoundException e) {
-            System.err.println("Output file not found: " + outputFileName);
-            System.exit(1);
-        }
-
-        // Process the input file line by line.
-        while (scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            String[] parts = line.split(" ");
-
-            // Format the name.
-            StringBuilder formattedName = new StringBuilder();
-            for (int i = 0; i < parts.length - 1; i++) {
-                String part = parts[i];
-                if (isUpperCase) {
-                    formattedName.append(part.toUpperCase().charAt(0));
-                    formattedName.append(part.substring(1).toUpperCase());
-                } else {
-                    formattedName.append(part.substring(0, 1).toUpperCase());
-                    formattedName.append(part.substring(1).toLowerCase());
+            while ((line = reader.readLine()) != null) {
+                String[] tokens = line.split(" ");
+                StringBuilder nameBuilder = new StringBuilder();
+                for (int i = 0; i < tokens.length - 1; i++) {
+                    String token = tokens[i];
+                    nameBuilder.append(token.substring(0, 1).toUpperCase());
+                    nameBuilder.append(token.substring(1));
+                    if (i < tokens.length - 2) {
+                        nameBuilder.append(" ");
+                    }
                 }
-                formattedName.append(" ");
+                String name = nameBuilder.toString();
+                String dob = tokens[tokens.length - 1];
+                String formattedDob = dob.substring(0, 2) + "/" + dob.substring(2, 4) + "/" + dob.substring(4);
+                if (args.length > 0 && args[0].equals("-u")) {
+                    name = name.toUpperCase();
+                }
+                writer.write(name + " " + formattedDob + "\n");
             }
-
-            // Format the date.
-            String date = parts[parts.length - 1];
-            String formattedDate = date.substring(0, 2) + "/" + date.substring(2, 4) + "/" + date.substring(4);
-
-            // Write the formatted line to the output file.
-            writer.println(formattedName.toString().trim() + " " + formattedDate);
+        } catch (IOException e) {
+            System.out.println("Error reading or writing file.");
+        } finally {
+            try {
+                reader.close();
+                writer.close();
+            } catch (IOException e) {
+                System.out.println("Error closing file.");
+            }
         }
-
-        // Clean up resources.
         scanner.close();
-        writer.close();
-
-    } // main
-
-} // FilesInOut
+    }
+}
