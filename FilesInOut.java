5bd27dcd1262ac53c3a754f9d329d11982a528e4
louiefk <lfk00001@students.stir.ac.uk>
2023-04-21 03:52:09 +0100

    Task One

--- FilesInOut.java
+++ FilesInOut.java
@@ -1,9 +1,5 @@
 import java.io.*;
-import java.awt.*;
-import java.awt.event.*;
 import java.util.*;
-import javax.swing.*;
-import java.lang.Number;
 
 /**
  * 
@@ -13,21 +9,71 @@ import java.lang.Number;
 public class FilesInOut {
 
     public static void main(String[] args) {
-        // Replace this with statements to set the file name (input) and file name (output).
-        // Initially it will be easier to hardcode suitable file names.
+        String inputFileName, outputFileName;
+        boolean isUpperCase = false;
+
+        // Check command line arguments.
+        if (args.length < 2 || args.length > 3) {
+            System.err.println("Usage: java FilesInOut [-u] input.txt output.txt");
+            System.exit(1);
+        }
+
+        int argIndex = 0;
+        if (args.length == 3 && args[argIndex].equals("-u")) {
+            isUpperCase = true;
+            argIndex++;
+        }
+        inputFileName = args[argIndex++];
+        outputFileName = args[argIndex];
 
         // Set up a new Scanner to read the input file.
-        // Processing line by line would be sensible here.
-        // Initially, echo the text to System.out to check you are reading correctly.
-        // Then add code to modify the text to the output format.
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(new File(inputFileName));
+        } catch (FileNotFoundException e) {
+            System.err.println("Input file not found: " + inputFileName);
+            System.exit(1);
+        }
 
         // Set up a new PrintWriter to write the output file.
-        // Add suitable code into the above processing (because you need to do this line by line also.
-        // That is, read a line, write a line, loop.
+        PrintWriter writer = null;
+        try {
+            writer = new PrintWriter(new File(outputFileName));
+        } catch (FileNotFoundException e) {
+            System.err.println("Output file not found: " + outputFileName);
+            System.exit(1);
+        }
 
-        // Finally, add code to read the filenames as arguments from the command line.
+        // Process the input file line by line.
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] parts = line.split(" ");
 
-        System.out.println("You need to add your own code to do anything");
+            // Format the name.
+            StringBuilder formattedName = new StringBuilder();
+            for (int i = 0; i < parts.length - 1; i++) {
+                String part = parts[i];
+                if (isUpperCase) {
+                    formattedName.append(part.toUpperCase().charAt(0));
+                    formattedName.append(part.substring(1).toUpperCase());
+                } else {
+                    formattedName.append(part.substring(0, 1).toUpperCase());
+                    formattedName.append(part.substring(1).toLowerCase());
+                }
+                formattedName.append(" ");
+            }
+
+            // Format the date.
+            String date = parts[parts.length - 1];
+            String formattedDate = date.substring(0, 2) + "/" + date.substring(2, 4) + "/" + date.substring(4);
+
+            // Write the formatted line to the output file.
+            writer.println(formattedName.toString().trim() + " " + formattedDate);
+        }
+
+        // Clean up resources.
+        scanner.close();
+        writer.close();
 
     } // main
 
